4021db243a7bab121ac8d75d49780dc897322e17
Evgeny Sugakov <shiroginne@gmail.com>
2018-03-27 13:23:10 +0200

    PackageFetcher fetch actual dependencies

--- lib/packages_fetcher.rb
+++ lib/packages_fetcher.rb
@@ -1,14 +1,25 @@
+require 'open-uri'
+
 class PackagesFetcher
   def self.fetch(name)
     fetcher = new(name)
-    fetcher.fetch
+    fetcher.dependencies
   end
+
+  attr_reader :name
 
   def initialize(name)
     @name = name
   end
 
-  def fetch
-    ["cliff"]
+  def dependencies
+    response = fetch
+    response['dependencies'].keys
   end
+
+  private
+    def fetch
+      url = "http://registry.npmjs.org/#{name}/latest"
+      JSON.parse URI(url).read
+    end
 end

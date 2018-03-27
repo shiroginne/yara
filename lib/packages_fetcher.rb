c1327afa56b5fc08ac7956d0a9eb776a97a6d261
Evgeny Sugakov <shiroginne@gmail.com>
2018-03-27 13:30:22 +0200

    Fetch dependencies of dependencies

--- lib/packages_fetcher.rb
+++ lib/packages_fetcher.rb
@@ -13,12 +13,18 @@ class PackagesFetcher
   end
 
   def dependencies
-    response = fetch
-    response['dependencies'].keys
+    dependencies = []
+    response = fetch(name)
+    deps = response['dependencies'].keys
+
+    deps.each { |dep| dependencies << fetch(dep)["dependencies"]&.keys }
+    dependencies << deps
+    
+    dependencies.flatten.compact
   end
 
   private
-    def fetch
+    def fetch(name)
       url = "http://registry.npmjs.org/#{name}/latest"
       JSON.parse URI(url).read
     end
--- spec/lib/packages_fetcher_spec.rb
+++ spec/lib/packages_fetcher_spec.rb
@@ -4,10 +4,16 @@ RSpec.describe PackagesFetcher do
   describe '.fetch' do
     subject { described_class }
 
-    it 'fetchers npm packages' do
+    it 'fetches npm packages' do
       dependencies = subject.fetch("forever")
 
       expect(dependencies).to include("cliff")
+    end
+
+    it 'fetches dependencies of dependencies' do
+      sub_dependencies = subject.fetch("forever")
+
+      expect(sub_dependencies).to include("eyes")
     end
   end
 end
